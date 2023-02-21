56cab3878245300784bfcd0db724a2ff209effeb
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-21 22:32:34 +0600

    добавлены в модель тодо два поля is_deleted, at_deleted

--- source/TODO_app/models.py
+++ source/TODO_app/models.py
@@ -12,3 +12,5 @@ class TODO(models.Model):
     description = models.TextField(max_length=2000, default='', verbose_name='Описание')
     status = models.CharField(max_length=12, choices=STATUS_CHOICES, default='new', verbose_name='Статус')
     date_of_completion = models.DateField(blank=True, null=True, default=None, verbose_name='Дата завершения')
+    is_deleted = models.BooleanField(default=False)
+    at_deleted = models.DateTimeField(null=True, blank=True)
