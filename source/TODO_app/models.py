55823be92b33c19c84ce9c38a01cb8fbb91279e2
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-21 21:40:25 +0600

    копирование модели тодо

--- source/TODO_app/models.py
+++ source/TODO_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class TODO(models.Model):
+    STATUS_CHOICES = [
+        ('new', 'Новая'),
+        ('in_progress', 'В процессе'),
+        ('completed', 'Завершено')
+    ]
+    title = models.CharField(max_length=64, null=False, verbose_name='Заголовок')
+    description = models.TextField(max_length=2000, default='', verbose_name='Описание')
+    status = models.CharField(max_length=12, choices=STATUS_CHOICES, default='new', verbose_name='Статус')
+    date_of_completion = models.DateField(blank=True, null=True, default=None, verbose_name='Дата завершения')
