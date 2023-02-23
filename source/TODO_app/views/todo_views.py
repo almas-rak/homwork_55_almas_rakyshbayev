20d8fdd304a652ce052695217ffd0be38fe29bf7
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-23 22:45:53 +0600

    добавленно тестовый метод создания задачи

--- source/TODO_app/views/todo_views.py
+++ source/TODO_app/views/todo_views.py
@@ -1,11 +1,25 @@
 from django.core.handlers.wsgi import WSGIRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from TODO_app.forms import TodoForm
 from TODO_app.models import TODO
 
 
 def view_todos(request: WSGIRequest):
     todos = TODO.objects.exclude(is_deleted=True)
     return render(request, 'index.html', context={'todos': todos})
+
+
+def create_todo(request: WSGIRequest):
+    if request.method == 'GET':
+        form = TodoForm()
+        return render(request, 'create_todo.html', context={'form': form})
+    else:
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todos')
+        else:
+            return render(request, 'create_todo.html', context={'form': form})
 
 
