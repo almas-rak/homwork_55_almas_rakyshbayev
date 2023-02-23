adef7447c3bf0e79af36ed1e6feff242680a0ec0
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-24 01:59:37 +0600

    в моделе todo переопределён метод удаления

--- source/TODO_app/models.py
+++ source/TODO_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
@@ -14,6 +15,11 @@ class TODO(models.Model):
     date_of_completion = models.DateField(blank=True, null=True, default=None, verbose_name='Дата завершения')
     is_deleted = models.BooleanField(default=False)
     at_deleted = models.DateTimeField(null=True, blank=True)
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_deleted = True
+        self.at_deleted = timezone.now()
+        self.save()
 
     def __str__(self):
         return self.title
