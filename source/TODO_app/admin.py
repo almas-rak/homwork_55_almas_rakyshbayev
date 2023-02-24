569d99cfe5020f2742298ad59bdb867b7dc484a6
Almas Rakyshbaev <kasper10.0711@gmail.com>
2023-02-24 18:58:18 +0600

    добавление TODO в админку

--- source/TODO_app/admin.py
+++ source/TODO_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from TODO_app.models import TODO
+
+
 # Register your models here.
+
+
+class TODOAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'status', 'date_of_completion', 'is_deleted', 'at_deleted')
+    list_editable = ('title', 'status', 'date_of_completion', 'is_deleted')
+
+
+admin.site.register(TODO, TODOAdmin)
